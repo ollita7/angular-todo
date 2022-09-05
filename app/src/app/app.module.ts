7a9c5a9160781f0e6a6b39dbb0fb5355d96c184c
Guillermo Fernandez <olla@MacBook-Pro-3.local>
2022-09-05 15:33:42 -0300

    small change

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TodoTaskComponent } from './todo-task/todo-task.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { TodoTaskComponent } from './todo-task/todo-task.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
